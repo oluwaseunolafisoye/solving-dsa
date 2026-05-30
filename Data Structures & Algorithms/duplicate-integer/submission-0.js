a91a0e6b51a991228eb2cb4f9c1c57f76a936bab
Oluwaseun Olafisoye <86838623+oluwaseunolafisoye@users.noreply.github.com>
2026-05-30 02:25:33 +0100

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const sortedNums = nums.sort((a, b) => a - b)
+
+        for (let i = 0; i < sortedNums.length; i++) {
+            if (sortedNums[i] === sortedNums[i + 1]) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
